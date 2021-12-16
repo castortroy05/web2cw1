16102841c3204cc1a4d39814c44186179090ea42
castortroy05 <44813655+castortroy05@users.noreply.github.com>
2021-12-16 15:09:16 +0000

    fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Main";
 import "./index.css";
-import 'mapbox-gl/dist/mapbox-gl.css';
 <link href="https://raw.githubusercontent.com/kartik-v/bootstrap-star-rating/master/css/star-rating.min.css" media="all" rel="stylesheet" type="text/css" />
 
 
